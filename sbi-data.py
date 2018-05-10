6988830f284b7f6ec27660d7413b88b2bb36a887
Prachi <prachi@LYS.prachi-lys>
2018-05-10 16:36:57 +0530

    Test Script for Scrapping Websites Tabular data into csv file.

--- sbi-data.py
+++ sbi-data.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import urllib
 import requests
@@ -15,11 +16,16 @@ soup = make_soup("https://in.finance.yahoo.com/quote/SBIN.BO/history?p=SBIN.BO")
 for record in soup.find_all('tr'):
 	finance_data = ""
 	for data in record.find_all('td'):
-		finance_data = finance_data+ ", " + data.text
-	
-	saved_finance_data = saved_finance_data + "\n" + finance_data[1:]
+		for text_data in data.find_all('span'):			
+			if "," in text_data.text:
+				text_data = text_data.text.replace(",", "")
+				finance_data = finance_data+ "," + text_data		
+			else:
+				finance_data = finance_data+ "," + (text_data.text)
+	if len(finance_data) <= 96 :				
+		saved_finance_data = saved_finance_data + "\n" + finance_data[1:]
 
-header_row = "Date,Open,High,Low,Close,Adj.Close,Volume" + "\n"
+header_row = "Date,Open,High,Low,Close,Adj.Close,Volume" 
 output_csv = open(os.path.expanduser("/home/prachi/Desktop/SBI_data.csv"), "wb")
 output_csv.write(bytes(header_row, encoding ="ascii", errors = "ignore"))
 output_csv.write(bytes(saved_finance_data, encoding ="ascii", errors = "ignore"))
